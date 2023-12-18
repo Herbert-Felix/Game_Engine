f4478fc86e266644b4f4ecafe567bc19a3c0f99f
Herbert-Felix <herbertfelix.dev@outlook.com>
2023-12-18 00:52:27 -0300

    Criado janela gréfica da game engine

--- game_engine/src/game_engine/Game.java
+++ game_engine/src/game_engine/Game.java
@@ -1,10 +1,35 @@
 package game_engine;
 
-public class Game {
+import java.awt.Canvas;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable {
+	
+	public static int WIDTH=480, HEIGHT=480;
+	
+	public Game(){
+		this.setPreferredSize(new Dimension(WIDTH,HEIGHT));
+	}
 
 	public static void main(String[] args) {
-		System.out.println("testando push Git");
+		Game game = new Game();
+		JFrame jframe = new JFrame();
+		jframe.add(game);
+		jframe.setTitle("Game Engine");
+		jframe.setLocationRelativeTo(null);
+		jframe.pack();
+		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jframe.setVisible(true);
+		new Thread(game).start();
+	}
 
+	@Override
+	public void run() {
+		while(true) {
+			System.out.println("Testando game loop frame...");
+		}
+		
 	}
 
 }
--- game_engine/src/module-info.java
+++ game_engine/src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module game_engine {
+	requires java.desktop;
 }
